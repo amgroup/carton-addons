56252d25bb95c06872b06463d737b72ff5ea3acf
Andrey <root@vmi12791.contabo.net>
2013-09-27 15:37:28 +0400

    added delivery via shoplogistic

---
+++ url_to_lowercase.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: Lowercase permalinks
+Description: A plugin to make lowercase permalinks
+Version: 1.0
+Author: Andrey Guryev
+Author URI: http://carton-ecommerce.com
+*/
+
+function array_strtolower($m=array()){return strtolower($m[0]);}
+function array_strtoupper($m=array()){return strtoupper($m[0]);}
+
+function correct_encoded_cyr_permalink ( $permalink ) {
+    $permalink = utf8_uri_encode( rawurldecode( $permalink ) );
+    $permalink = preg_replace_callback( "/(%[a-f0-9]{2})+/", 'array_strtoupper', $permalink );
+    return $permalink;
+}
+
+foreach( array( 'clean_url', 'page_link', 'post_link', 'term_link', 'home_url' ) as $filter ) {
+    add_filter($filter, 'correct_encoded_cyr_permalink' );
+}
+
+?>
